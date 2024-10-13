095d109f323e8cf969fd2908c7b27024ee807548
RoshinM <roshinmaharjan7@gmail.com>
2024-10-13 13:39:50 +0545

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lab1/pages/LoginPage.dart';
+import 'package:lab1/pages/dashboard.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      title: "Flutter Demo",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: const dashboardPage(),
     );
   }
 }
